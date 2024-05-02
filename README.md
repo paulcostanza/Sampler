c4f437210f1ac61f56e5904b9f38f3452382e618
Paul Costanza <paulvincentcostanza@gmail.com>
2024-05-02 12:06:33 -0400

    Change sampler layout, added mouse click, cleaned up look

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 New name for project: beat laboratory 
 
 
-- The tapping with the keyboard sucks. Definitely need to switch to the mouse clicks, eventually touch (for phone)
-- Add an option for left handed or right handed
-- Drums on one side, cymbals on the other
-- light up drum when pad is hit, maybe use adobe for cool lighting effect?
+- The tapping with the keyboard sucks. Definitely need to switch to the mouse clicks for desktop
 - click and drag to put into a new spot?
 - need to add onclick
-- need to figure out how to stop *open hi-hat* when you hit *close hi hat*
+- need to figure out how to stop *open hi-hat* when you hit *close hi hat*. Also add a close when you lift a finger off the close hi-hat (so you can do a drag effect)
+- dang it, I'm gonna add the keyboard back in lol
--- main.js
+++ main.js
@@ -9,6 +9,11 @@ window.addEventListener('touchstart', function (e) {
 
 });
 
+// to stop the open hi hat when the user hits close hi hat
+function stopAudio(audio) {
+    audio.pause()           // pause the audio (because there is no built-in stop function)
+    audio.currentTime = 0   // reset the playback position to the beginning
+}
 
 
 // Stop the CSS transition
@@ -48,34 +53,53 @@ const snare = document.getElementById('snare')
 const hiHatOpen = document.getElementById('hi-hat-open')
 const hiHatClose = document.getElementById('hi-hat-close')
 
-
-bass.addEventListener('touchstart', e => {
+// Bass Drum
+function playBass() {
     const audio = new Audio()
     audio.src = "sounds/bass_drum.wav"
     audio.play()
     bass.classList.add('playing');
-})
+}
 
-snare.addEventListener('touchstart', e => {
+bass.addEventListener('click', playBass)
+bass.addEventListener('touchstart', playBass)
+bass.addEventListener(new KeyboardEvent('keypress', { 'key': 'h' }), playBass)
+
+// Snare Drum
+function playSnare() {
     const audio = new Audio()
     audio.src = "sounds/snare.wav"
     audio.play()
     snare.classList.add('playing');
-})
+}
 
-hiHatOpen.addEventListener('touchstart', e => {
-    const audio = new Audio()
-    audio.src = "sounds/hi-hat_open.wav"
-    audio.play()
+snare.addEventListener('click', playSnare)
+snare.addEventListener('touchstart', playSnare)
+
+// Hi Hats
+let hihatAudio
+
+function playOpenHiHat() {
+    hihatAudio = new Audio()
+    hihatAudio.src = "sounds/hi-hat_open.wav"
+    hihatAudio.play()
     hiHatOpen.classList.add('playing');
-})
+}
 
-hiHatClose.addEventListener('touchstart', e => {
+hiHatOpen.addEventListener('click', playOpenHiHat)
+hiHatOpen.addEventListener('touchstart', playOpenHiHat)
+
+function playCloseHiHat() {
     const audio = new Audio()
     audio.src = "sounds/hi-hat_close.wav"
     audio.play()
     hiHatClose.classList.add('playing');
-})
+
+    stopAudio(hihatAudio)
+}
+
+hiHatClose.addEventListener('click', playCloseHiHat)
+hiHatClose.addEventListener('touchstart', playCloseHiHat)
 
 
 
@@ -84,7 +108,7 @@ hiHatClose.addEventListener('touchstart', e => {
 
 
 document.addEventListener("touchmove", e => {
-    console.log("Move")
+    //console.log("Move")
 })
 
 document.addEventListener("touchend", e => {
